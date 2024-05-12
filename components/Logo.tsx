d01e4a80f0b84c366b6851edf369b2f91af9f5b8
sami shaikh <samishaikh05687@gmail.com>
2024-05-13 00:01:42 +0530

    UI changes

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ function Logo() {
   return (
     <Link
       href={"/"}
-      className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer"
+      className="font-bold text-3xl bg-gradient-to-r from-purple-900 to-purple-400 bg-clip-text text-transparent  hover:cursor-pointer"
     >
       Formify360
     </Link>
